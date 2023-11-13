b3d01f89d71c0da7fc13be2ede2fa0b260e14500
Greg Pappas <gnpappas@proton.me>
2023-11-13 11:18:09 -1000

    Improve style readability

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 const Navbar = () => {
 	return (
-		<nav className="flex justify-center pb-4">
+		<nav className="max-w-2xl flex justify-center mx-auto bg-green-200/90 rounded-md mb-2 border border-green-500 shadow-md">
 			<ul className="flex">
 				<CustomLink to="/">Home</CustomLink>
 				<CustomLink to="/services">Services</CustomLink>
@@ -16,14 +16,12 @@ const Navbar = () => {
 	function CustomLink({ to, children }) {
 		const resolvedPath = useResolvedPath(to);
 		const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+		const activeStyle =
+			"font-bold px-3 py-2 rounded-sm bg-green-100/60 text-green-700 shadow-[0px_0px_3px_green]";
+		const inactiveStyle =
+			"font-bold px-3 py-2 text-slate-800 rounded-lg hover:text-green-700";
 		return (
-			<li
-				className={
-					isActive
-						? "font-bold px-3 py-2 rounded-lg bg-green-100 text-green-800 m-1"
-						: "font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-green-200 hover:text-green-800 m-1"
-				}
-			>
+			<li className={isActive ? activeStyle : inactiveStyle}>
 				<Link to={to}>{children}</Link>
 			</li>
 		);
