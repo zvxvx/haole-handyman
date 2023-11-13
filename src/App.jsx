b68756de9f111b5111776e5286f7462605af851c
Greg Pappas <gnpappas@proton.me>
2023-11-13 11:17:54 -1000

    Improve layout UX

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,28 @@ import Services from "./pages/Services";
 import Testimonials from "./pages/Testimonials";
 import { Route, Routes } from "react-router-dom";
 import logo from "./assets/logo.png";
+import bg from "./assets/bg.jpg";
 
 function App() {
 	return (
-		<div className="min-h-screen bg-green-300">
-			<img src={logo} className="mx-auto my-auto w-1/6" id="logo" alt="logo" />
+		<div
+			className="min-h-screen bg-green-300 pb-5"
+			style={{
+				backgroundImage: `url(${bg})`,
+				backgroundRepeat: "no-repeat",
+				backgroundSize: "cover",
+			}}
+		>
+			<img
+				src={logo}
+				className="mx-auto my-auto w-1/6"
+				id="logo"
+				alt="logo"
+			/>
 			<Navbar />
-			<div className="mx-auto my-auto">
+			<div
+				className="flex-col max-w-2xl mx-auto rounded-md shadow-xl items-center justify-center border border-green-500 bg-green-200/90 p-8"
+			>
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/services" element={<Services />} />
