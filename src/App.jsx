15bda786509de84fb44beddf286e5e5f212234a3
Greg Pappas <gnpappas@proton.me>
2023-11-12 20:53:03 -1000

    Remove Quote page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import Navbar from "./Navbar";
 import Home from "./pages/Home";
-import Quote from "./pages/Quote";
 import Services from "./pages/Services";
 import Testimonials from "./pages/Testimonials";
 import { Route, Routes } from "react-router-dom";
+import logo from "./assets/logo.png";
 
 function App() {
 	return (
-		<>
+		<div className="h-screen bg-gradient-to-r from-green-300 to-green-200">
+			<img src={logo} className="mx-auto my-auto w-1/6" id="logo" alt="logo" />
 			<Navbar />
-			<div id="container">
+			<div className="mx-auto my-auto">
 				<Routes>
 					<Route path="/" element={<Home />} />
-					<Route path="/quote" element={<Quote />} />
 					<Route path="/services" element={<Services />} />
 					<Route path="/testimonials" element={<Testimonials />} />
 				</Routes>
 			</div>
-		</>
+		</div>
 	);
 }
 
