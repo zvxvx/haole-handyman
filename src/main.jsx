6b51527018e277e3a519e8ec157f4ed226871081
Greg Pappas <gnpappas@proton.me>
2023-11-12 21:30:19 -1000

    Have output in gitignore

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./output.css";
+import "../dist/output.css";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
