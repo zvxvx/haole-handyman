6e0e01df420ccb75977afb2249f71d773b53c117
Greg Pappas <gnpappas@proton.me>
2023-11-14 04:56:33 -1000

    Remove logo with placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import Home from "./pages/Home";
 import Services from "./pages/Services";
 import Testimonials from "./pages/Testimonials";
 import { Route, Routes } from "react-router-dom";
-import logo from "./assets/logo.png?w=250&h=250&format=webp";
 
 function App() {
 	return (
 		<div className="min-h-screen bg-gradient-to-t from-emerald-400 to-green-200">
-			<img src={logo} className="mx-auto my-auto p-2" id="logo" alt="logo" />
+			<div className="sm:max-w-2xl mx-auto px-2 pt-5">
+				<ul className="flex justify-between font-bold uppercase text-green-900 text-xl sm:text-2xl">
+					<li>Haole Handyman</li>
+					<li>
+						<a href="tel:+1808-721-4647">808-721-4647</a>
+					</li>
+				</ul>
+			</div>
 			<Navbar />
 			<div className="sm:max-w-2xl flex-col mx-auto rounded-md shadow-xl items-center justify-center border border-green-500 bg-gradient-to-t from-green-200/50 to-green-100/50 p-6">
 				<Routes>
