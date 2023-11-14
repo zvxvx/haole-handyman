2efc6a0aa42fb8df77000ceff958c874a67b7be9
Greg Pappas <gnpappas@proton.me>
2023-11-13 19:31:18 -1000

    Run test for absolute background color

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../src/output.css";
+import "./index.css";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
