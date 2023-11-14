8e79e1848dfd68c1fe72b533008b3b1daff862bd
Greg Pappas <gnpappas@proton.me>
2023-11-13 19:06:26 -1000

    Add image-tools

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { imagetools } from "vite-imagetools";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react(), imagetools()],
+});
