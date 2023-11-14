c2bf20e789bb872189a51bb0838d108032914640
Greg Pappas <gnpappas@proton.me>
2023-11-13 17:09:56 -1000

    Update footer

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 		<div className="min-h-screen bg-gradient-to-r from-green-300 to-green-200">
 			<img src={logo} className="mx-auto my-auto w-[50%] sm:w-64" id="logo" alt="logo" />
 			<Navbar />
-			<div className="flex-col max-w-2xl mx-auto rounded-md shadow-xl items-center justify-center border border-green-500 bg-green-200/90 p-6">
+			<div className="flex-col max-w-2xl mx-auto rounded-md shadow-xl items-center justify-center border border-green-500 bg-gradient-to-t from-green-200/50 to-green-100/70 p-6">
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/services" element={<Services />} />
@@ -19,9 +19,9 @@ function App() {
 
 			</div>
 			<footer className="w-full mt-2 flex items-center justify-center pb-2 text-sm text-gray-900 sm:text-center">
-				<span>{new Date().toLocaleString().slice(6, 10) + "/design/"}</span>
-				<a href="https://pappas.sh" className="font-bold" target="_blank">
-					pappas
+				<span className="underline decoration-green-700">{new Date().toLocaleString().slice(6, 10)}</span>
+				<a href="https://pappas.sh" className="font-bold underline decoration-green-800" target="_blank">
+					/pappas
 				</a>
 			</footer>
 		</div>
