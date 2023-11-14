0f054229383b849c78ba2d15d613ce13fa4429f0
Greg Pappas <gnpappas@proton.me>
2023-11-13 14:46:31 -1000

    Consolidate services

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -6,39 +6,28 @@ import flooring from "../assets/services/flooring.jpg";
 import kitchen from "../assets/services/kitchen.jpg";
 import toilet from "../assets/services/toilet.jpg";
 
-const images1 = [ac, door, electrical];
-const images2 = [flooring, kitchen, toilet];
-const services1 = ["Cooling", "Doors", "Electrical"];
-const services2 = ["Flooring", "Kitchen", "Bathroom"];
+const images = [ac, door, electrical, flooring, kitchen, toilet];
+const services = [
+	"Cooling",
+	"Refurbishing",
+	"Electrical",
+	"Flooring",
+	"Kitchen",
+	"Bathroom",
+];
 const Services = () => {
 	return (
 		<>
-			<p className="text-center uppercase tracking-wide text-xl text-green-900 font-bold mb-5">
+			<p className="text-center uppercase tracking-wide text-xl text-green-900 font-bold mb-2">
 				Service examples include
 			</p>
-			<div className="grid gap-4 grid-cols-3 grid-rows-1 bg-green-100/50 border border-green-700 p-2 rounded shadow-xl mb-5">
-				{services1.map((service) => {
-					return (
-						<span className="border-b-2 border-green-800 font-bold uppercase text-center">
-							{service}
-						</span>
-					);
+			<ul className="sm:flex gap-4 justify-center text-center font-bold mb-2">
+				{services.map((service) => {
+					return <li>{service}</li>;
 				})}
-				{images1.map((image) => {
-					return (
-						<img className="border-2 border-black" src={image} alt="services" />
-					);
-				})}
-			</div>
-			<div className="grid gap-4 grid-cols-3 grid-rows-1 bg-green-100/50 border border-green-700 p-2 rounded shadow-xl">
-				{services2.map((service) => {
-					return (
-						<span className="border-b-2 border-green-800 font-bold uppercase text-center">
-							{service}
-						</span>
-					);
-				})}
-				{images2.map((image) => {
+			</ul>
+			<div className="sm:grid gap-4 grid-cols-3 grid-rows-1 bg-green-100/50 border border-green-700 p-4 rounded shadow-xl mb-5">
+				{images.map((image) => {
 					return (
 						<img className="border-2 border-black" src={image} alt="services" />
 					);
