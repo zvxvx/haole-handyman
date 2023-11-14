327243bd073dc7a879a456dbf2293d38aae9bc25
Greg Pappas <gnpappas@proton.me>
2023-11-13 19:06:19 -1000

    Adjust styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,28 @@ import Home from "./pages/Home";
 import Services from "./pages/Services";
 import Testimonials from "./pages/Testimonials";
 import { Route, Routes } from "react-router-dom";
-import logo from "./assets/logo.png";
+import logo from "./assets/logo.png?w=300&h=300&format=webp";
 
 function App() {
 	return (
-		<div className="min-h-screen bg-gradient-to-r from-green-300 to-green-200">
-			<img src={logo} className="mx-auto my-auto w-[50%] sm:w-64" id="logo" alt="logo" />
+		<div className="relative min-h-screen bg-gradient-to-r from-green-300 to-green-200">
+			<img src={logo} className="mx-auto my-auto mb-2" id="logo" alt="logo" />
 			<Navbar />
-			<div className="flex-col max-w-2xl mx-auto rounded-md shadow-xl items-center justify-center border border-green-500 bg-gradient-to-t from-green-200/50 to-green-100/70 p-6">
+			<div className="sm:max-w-2xl flex-col mx-auto rounded-md shadow-xl items-center justify-center border border-green-500 bg-gradient-to-t from-green-200/50 to-green-100/70 p-6">
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/services" element={<Services />} />
 					<Route path="/testimonials" element={<Testimonials />} />
 				</Routes>
-
 			</div>
-			<footer className="w-full mt-2 flex items-center justify-center pb-2 text-sm text-gray-900 sm:text-center">
-				<span className="underline decoration-green-700">{new Date().toLocaleString().slice(6, 10)}</span>
-				<a href="https://pappas.sh" className="font-bold underline decoration-green-800" target="_blank">
-					/pappas
+			<footer className="w-full mt-2 flex items-center justify-center pb-2 text-md sm:text-center">
+				<span>{new Date().toLocaleString().slice(6, 10) + "/"}</span>
+				<a
+					href="https://pappas.sh"
+					className="font-bold underline"
+					target="_blank"
+				>
+					pappas
 				</a>
 			</footer>
 		</div>
