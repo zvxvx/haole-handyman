8998893fa729d72a727298160ad50098bbaefb6d
Greg Pappas <gnpappas@proton.me>
2023-11-13 14:46:17 -1000

    Design alterations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
 		<>
 			<div id="home">
 				<img
-					className="drop-shadow-2xl mx-auto my-auto pt-5"
+					className="drop-shadow-2xl mx-auto my-auto pt-5 w-96"
 					src={owners}
 					id="owners"
 					alt="Jadyi and Brad"
@@ -22,13 +22,13 @@ const Home = () => {
 					<div className="uppercase tracking-wide text-lg text-green-800 font-bold">
 						About
 					</div>
-					<p className="pb-3">
+					<p className="bg-green-100 rounded-t-lg shadow-lg p-3">
 						Brad, originally from Texas, moved here in 2019 and fell in love
 						with the island life. He cares about providing quality work and
 						customer service. Just as important is his belief in giving fair
 						prices to the residents of O'ahu.
 					</p>
-					<p>
+					<p className="bg-green-100 rounded-b-lg shadow-lg p-3">
 						His partner, Jaydi, was born on Kaua'i, but was raised on O'ahu.
 						You'll likely hear her voice on the other end when you give Ha'ole
 						Handyman a call. They complement each other well and look forward to
@@ -37,19 +37,19 @@ const Home = () => {
 					<div className="uppercase tracking-wide text-lg text-green-800 font-bold mt-5">
 						Need a quote?
 					</div>
-					<p>
+					<div className="bg-green-100 rounded-lg shadow-lg p-3">
 						Provide us with your name, a good phone number or email to respond
 						to, and a description of what service you'd like to request. The
 						more information you provide us, the more accurate the quote will
 						be.
-					</p>
-					<button
-						type="button"
-						onClick={requestQuote}
-						className="font-bold px-3 py-2 rounded-lg bg-green-100 text-green-800 border border-green-500 shadow-md mt-5 mx-auto block"
-					>
-						Request Service Quote
-					</button>
+						<button
+							type="button"
+							onClick={requestQuote}
+							className="font-bold px-3 py-2 rounded-lg bg-green-200 text-green-800 border border-green-500 shadow-md mt-5 mx-auto block"
+						>
+							Request Service Quote
+						</button>
+					</div>
 				</div>
 			</div>
 		</>
