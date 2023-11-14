1b1153f01d6f559ac3821b4de89b5acf84f34a27
Greg Pappas <gnpappas@proton.me>
2023-11-13 19:20:22 -1000

    Re-add file for compilation

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../style/output.css";
+import "../src/output.css";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
