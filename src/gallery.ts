13211014c38894e822e11ec21bf090fab46f2f3d
Greg Pappas <gnpappas@proton.me>
2024-01-21 12:30:33 -1000

    Add preview images with links to full image

--- src/gallery.ts
+++ src/gallery.ts
@@ -1,14 +1,15 @@
 import type { ImageMetadata } from "astro";
-import ac from "./assets/gallery/ac.jpg";
-import door from "./assets/gallery/door.jpg";
-import electrical from "./assets/gallery/electrical.jpg";
-import floor from "./assets/gallery/flooring.jpg";
-import kitchen from "./assets/gallery/kitchen.jpg";
-import bathroom from "./assets/gallery/toilet.jpg";
+import ac from "./assets/gallery/ac.webp";
+import door from "./assets/gallery/door.webp";
+import electrical from "./assets/gallery/electrical.webp";
+import floor from "./assets/gallery/flooring.webp";
+import kitchen from "./assets/gallery/kitchen.webp";
+import bathroom from "./assets/gallery/toilet.webp";
 
 interface GalleryProps {
   alt: string;
   image: ImageMetadata;
+  full: string;
 }
 [];
 
@@ -16,25 +17,31 @@ export const IMG_GALLERY: GalleryProps[] = [
   {
     alt: "Cooling",
     image: ac,
+    full: "ac.jpg",
   },
   {
     alt: "Refurbishing",
     image: door,
+    full: "door.jpg",
   },
   {
     alt: "Electrical",
     image: electrical,
+    full: "electrical.jpg",
   },
   {
     alt: "Flooring",
     image: floor,
+    full: "floor.jpg",
   },
   {
     alt: "Kitchen",
     image: kitchen,
+    full: "kitchen.jpg",
   },
   {
     alt: "Bathroom",
     image: bathroom,
+    full: "bathroom.jpg",
   },
 ];
