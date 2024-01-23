dd1add842a624fbf2596d92ca7df209abf90eefd
Greg Pappas <gnpappas@proton.me>
2024-01-22 16:34:06 -1000

    Run test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { loadEnv } from "vite";
+
+const { PUBLIC_YELP } = loadEnv(process.env.PUBLIC_YELP, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
 });
