740d11b77e61d5aec6fd580919dcea32bda2fd19
Greg Pappas <gnpappas@proton.me>
2024-01-22 16:37:38 -1000

    Test different config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import { loadEnv } from "vite";
-
-const { PUBLIC_YELP } = loadEnv(process.env.PUBLIC_YELP, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
